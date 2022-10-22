abf2607ab38f0e103f7b4603bb9f6ff4a62fef70
Rafael Monterroza <rafaelenrike@uoc.edu>
2022-10-22 00:16:53 +0000

    First commit

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.28.0"
+    }
+  }
+}
+
+provider "azurerm" {
+   features {
+
+  }
+}
+
+resource "azurerm_resource_group" "rg_utbapp" {
+  name = "rg_utbapp" # this is the name on azure
+  location = "eastus" # data center location on azure
+}
+
+resource "azurerm_container_group" "tf_cg_utb" {
+  name                  = "utbapp"
+  location              = azurerm_resource_group.rg_utbapp.location #utilising the resource group
+  resource_group_name   = azurerm_resource_group.rg_utbapp.name #utilising the resource group
+
+  ip_address_type       = "Public"
+  dns_name_label        = "rafaelenrike-utb" #friendly name we want to give our domain
+  os_type               = "Linux"
+
+  # Specify the container information
+  container {
+    name = "utbapp"
+    image = "rafaelenrike/utbapp"
+    cpu = "1"
+    memory = "1"
+
+    ports {
+        port = 80
+        protocol = "TCP"
+    }
+  }
+}
