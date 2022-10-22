4f4242ae719faae1fc2204def5f96fc502b2009f
Rafael Monterroza <rafaelenrike@uoc.edu>
2022-10-22 14:33:30 +0000

    Modificacion del saludo 2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hola muchachos. Espero que les guste el tema");
+app.MapGet("/", () => "Hola muchachos. Espero que les guste el tema!");
 
 app.Run();
