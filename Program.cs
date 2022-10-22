cac07eb8fb78ba96b0896faaf025d17a9fb83176
Rafael Monterroza <rafaelenrike@uoc.edu>
2022-10-22 04:01:43 +0000

    Change Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "<h1><center>Infraestructura para TI</center></h1>");
 
 app.Run();
